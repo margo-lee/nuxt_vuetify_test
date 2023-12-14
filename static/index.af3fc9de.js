cea08ee0ec2b551aa78bf077c24e04ef9766d61a
margo-lee <margo.lee@hyweb.com.tw>
2023-12-14 14:40:27 +0800

    Updates

---
+++ static/index.af3fc9de.js
@@ -0,0 +1,1 @@
+import{c as e,o as t,a as o}from"./entry.fc7138c7.js";const c=o("h2",null,"product index",-1),n=[c],d={__name:"index",setup(s){return(a,_)=>(t(),e("div",null,n))}};export{d as default};
